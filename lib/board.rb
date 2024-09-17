9f645ebeb438131a2704fbff33bbd33b380b8df8
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-09-17 21:35:20 +0100

    Board class, shows board. Tests for proper functioning.

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,31 @@
 # board.rb
 # frozen_string_literal: true
 
+class Board
+  def initialize
+    @matrix = Array.new(BOARD_SIZE) { Array.new(BOARD_SIZE, "\u2B1C") }
+    @display = display
+  end
+
+  BOARD_SIZE = 8
+
+  def display
+    num = 8
+
+    puts
+    @matrix.each do |row|
+      puts "#{num -= 1} #{row.join}"
+    end
+
+    puts "  A B C D E F G H\n\n"
+  end
+
+  def change_board(cell_one, cell_two)
+    @matrix[7 - cell_one][cell_two] = "\u2B1B"
+    display
+  end
+end
+
+test = Board.new
+test.change_board(2, 5)
+test.change_board(3, 3)
