e007d8b6d674363c8adfbf19b6d95d9b5602c3e4
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-09-18 16:00:22 +0100

    Universally accepted RED_CIRCLE. Funtion to check if valid cell and swap letter to nums.

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,6 @@
 # board.rb
 # frozen_string_literal: true
 
-require 'rainbow/refinement'
-using Rainbow
-
 class Board
   def initialize
     @matrix = Array.new(BOARD_SIZE) { Array.new(BOARD_SIZE, WHITE_SQUARE) }
@@ -14,14 +11,38 @@ class Board
 
   BLACK_SQUARE = "\u2B1B"
   WHITE_SQUARE = "\u2B1C"
+  RED_CIRCLE = "\u2B55"
+
+  def change_board(cell_two, cell_one)
+    cell_two = letter_to_number(cell_two)
+
+    raise ArgumentError, 'Invalid cell input.' unless valid_cell?(cell_one) && valid_cell?(cell_two)
+
+    @matrix[8 - cell_one][cell_two] = RED_CIRCLE
+
+    display
+  end
+
+  private
+
+  def valid_cell?(cell)
+    cell >= 0 && cell < BOARD_SIZE
+  end
+
+  def letter_to_number(cell_two)
+    small_cell_two = cell_two.downcase.to_sym
+
+    change = { a: 0, b: 1, c: 2, d: 3, e: 4, f: 5, g: 6, h: 7 }
+    change[small_cell_two]
+  end
 
   def black_cells(num = 0)
     until num == BOARD_SIZE
 
       if num.even?
-        [1, 3, 5, 7].each { |i| @matrix[num][i] = BLACK_SQUARE unless @matrix[num][i] == '🔴' }
+        [1, 3, 5, 7].each { |i| @matrix[num][i] = BLACK_SQUARE unless @matrix[num][i] == RED_CIRCLE }
       else
-        [0, 2, 4, 6].each { |i| @matrix[num][i] = BLACK_SQUARE unless @matrix[num][i] == '🔴' }
+        [0, 2, 4, 6].each { |i| @matrix[num][i] = BLACK_SQUARE unless @matrix[num][i] == RED_CIRCLE }
       end
 
       num += 1
@@ -30,7 +51,7 @@ class Board
 
   def display
     black_cells
-    num = 8
+    num = 9
 
     puts
 
@@ -39,10 +60,5 @@ class Board
     end
 
     puts "\n   A B C D E F G H"
-  end
-
-  def change_board(cell_one, cell_two)
-    @matrix[7 - cell_one][cell_two] = '🔴'
-    display
   end
 end
